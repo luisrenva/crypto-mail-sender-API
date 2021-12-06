d26b0ad9208ab204e7e16855b9ea5686535234a5
Luis Renteria Valencia <luis@Luiss-MBP.attlocal.net>
2021-12-06 16:39:45 -0600

    adding more connections

--- email/send-email.js
+++ email/send-email.js
@@ -3,7 +3,15 @@ const nodemailer = require('nodemailer')
 
 exports.sendEmail = (price, cryptoName) => {
   const transporter = nodemailer.createTransport({
+    host: 'smtp-mail.outlook.com',
     service: 'outlook',
+    secureConnection: false,
+    tls: {
+        ciphers: 'SSLv3'                            // tls version
+    },
+    port: 587,
+    maxConnections: 3,
+    pool: true, 
     auth: {
       user: process.env.HOTMAIL_USER,
       pass: process.env.HOTMAIL_PWD
@@ -13,9 +21,11 @@ exports.sendEmail = (price, cryptoName) => {
   var mailOptions = {
     from: process.env.HOTMAIL_USER,
     to: process.env.EMAILS,
-    // to: constants.emails,
+    // to: 'luis.renva@gmail.com',
     subject: 'Changes in ' + cryptoName + '!!!!!!',
+    // subject: 'test',
     text: 'Current ' + cryptoName + ' price::::: ' + price
+    // text: 'test'
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
