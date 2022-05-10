4e13927b3830d2e91dc2e95f06d1ed44aad292c4
Luis Renteria Valencia <luis@Luiss-MBP.attlocal.net>
2022-05-10 17:09:46 -0500

    inetgrate with heroku cli

--- email/send-email.js
+++ email/send-email.js
@@ -45,15 +45,6 @@ exports.sendEmail = (body) => {
     console.log(error ? error : 'Email sent: ' + info.response)
   });
 
-
-  // transporter.verify(function (error, success) {
-  //   if (error) {
-  //     console.log(error);
-  //   } else {
-  //     console.log("Server is ready to take our messages");
-  //   }
-  // });
-
   // nodeoutlook.sendEmail({
   //   pool: true,
   //   maxConnections: 8,
